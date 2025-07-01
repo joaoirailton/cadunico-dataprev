5c28aad49de9ffab8bd029ebfdd2bb55513b1799
joaoirailton <junioramos4991@gmail.com>
2025-07-01 11:52:40 -0300

    implementação

---
+++ javascript/script-page-buscar.js
@@ -0,0 +1,13 @@
+//DECLARAÇÃO DE CONSTANTES
+const abaBuscar = document.querySelectorAll('.t-busca');
+const formulario = document.querySelector('#box-formulario');
+
+//ADICIONA EVENTO
+for (let indice = 0; indice <= abaBuscar.length; indice ++){
+    abaBuscar[indice].addEventListener('click', () => abriFecha(indice));
+}
+
+//FUNÇÕES
+function abriFecha(indice){
+    formulario.classList.toggle('ativo');
+}
